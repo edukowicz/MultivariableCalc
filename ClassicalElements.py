18f9be47a3e71e91afc67a1a43c583fe33a3458a
edukowicz <26.ellie.dukowicz@carolinaday.org>
2025-11-06 17:12:01 +0000

    1CQuiz

--- ClassicalElements.py
+++ ClassicalElements.py
@@ -1,61 +1,68 @@
 import MultivariableCalc as calc
 import math
 
-#define important known vectors
-r = [2615, 15881, 3980]
-v = [-2.767, -0.7905, 4.980]
-i = [1, 0, 0]
-j = [0, 1, 0]
-k = [0, 0, 1]
+def vecH(r, v):
+    hvec = calc.cross_product(r, v)
+    return hvec
 
-#find vector h
-hvec = calc.cross_product(r, v)
-h = calc.vector_magnitude(hvec)
-print(f"h = {h} km^2/s")
-print()
+def magH(r, v):
+    h = calc.vector_magnitude(vecH(r, v))
+    return h
 
-#find vector e
-mew = 398600
-runit = calc.unit_vector(r)
-evec = calc.scalar_multiply(calc.vector_operation(calc.cross_product(v, hvec), calc.scalar_multiply(runit, mew), "subtraction"),(1/mew))
-e = calc.vector_magnitude(evec)
-print(f"e = {e}")
-print()
+def vecE(r, v):
+    mew = 398600
+    runit = calc.unit_vector(r)
+    hvec = vecH(r, v)
+    evec = calc.scalar_multiply(calc.vector_operation(calc.cross_product(v, hvec), calc.scalar_multiply(runit, mew), "subtraction"),(1/mew))
+    return evec
 
-#find vector for node line
-node = calc.cross_product(k, hvec)
+def magE(r, v):
+    e = calc.vector_magnitude(vecE(r, v))
+    return e
 
-#find angle i
-eqplane = calc.cross_product(node, k)
-oplane = calc.cross_product(node, hvec)
-eqdoto = calc.dot_product(eqplane, oplane)
-eqmag = calc.vector_magnitude(eqplane)
-omag = calc.vector_magnitude(oplane)
-irad = math.acos(eqdoto/(eqmag*omag))
-iangle = math.degrees(irad)
-print(f"i = {iangle} degrees")
-print()
+def node(r, v):
+    hvec = vecH(r, v)
+    k = [0, 0, 1]
+    nodeLine = calc.cross_product(k, hvec)
+    return nodeLine
 
+def angleI(r, v):
+    nodeLine = node(r, v)
+    hvec = vecH(r, v) 
+    k = [0, 0, 1]
+    eqplane = calc.cross_product(nodeLine, k)
+    oplane = calc.cross_product(nodeLine, hvec)
+    eqdoto = calc.dot_product(eqplane, oplane)
+    eqmag = calc.vector_magnitude(eqplane)
+    omag = calc.vector_magnitude(oplane)
+    irad = math.acos(eqdoto/(eqmag*omag))
+    iangle = math.degrees(irad)
+    return iangle
 
-#find angle omega
-nodedoti = calc.dot_product(node, i)
-nodemag = calc.vector_magnitude(node)
-omegarad = math.acos(nodedoti/nodemag)
-omega = math.degrees(omegarad)
-print(f"omega = {omega} degrees")
-print()
+def angleOmega(r, v):
+    nodeLine = node(r, v)
+    i = [1, 0, 0]
+    nodedoti = calc.dot_product(nodeLine, i)
+    nodemag = calc.vector_magnitude(nodeLine)  
+    omegarad = math.acos(nodedoti/nodemag)
+    omega = math.degrees(omegarad)
+    return omega
 
-#find angle theta
-rdote = calc.dot_product(r, evec)
-rmag = calc.vector_magnitude(r)
-emag = calc.vector_magnitude(evec)
-thetarad = math.acos(rdote/(rmag*emag))
-theta = math.degrees(thetarad)
-print(f"theta = {theta} degrees")
-print()
+def angleTheta(r, v):
+    evec = vecE(r, v)
+    rdote = calc.dot_product(r, evec)
+    rmag = calc.vector_magnitude(r)
+    emag = calc.vector_magnitude(evec)
+    thetarad = math.acos(rdote/(rmag*emag))
+    theta = math.degrees(thetarad)
+    return theta
 
-#find angle w
-nodedote = calc.dot_product(node, evec)
-wrad = math.acos(nodedote/(emag*nodemag))
-w = math.degrees(wrad)
-print(f"w = {w} degrees")
+def angleW(r, v):
+    nodeLine = node(r, v)
+    evec = vecE(r, v)
+    nodedote = calc.dot_product(nodeLine, evec)
+    nodemag = calc.vector_magnitude(nodeLine)
+    emag = calc.vector_magnitude(evec)
+    wrad = math.acos(nodedote/(emag*nodemag))
+    w = math.degrees(wrad)
+    return w
---
+++ CoordinateTransformations.py
@@ -0,0 +1,57 @@
+import MultivariableCalc as calc
+import math
+import ClassicalElements
+
+r = [4623.458, 5533.656, 4646.733]
+v = [-6.033, 3.485, 4.480]
+
+#find vector h
+h = ClassicalElements.magH(r, v)
+print(f"h = {h} km^2/s")
+print()
+
+#find vector e
+e = ClassicalElements.magE(r, v)
+print(f"e = {e}")
+print()
+
+#find angle i
+iangle = ClassicalElements.angleI(r, v)
+print(f"i = {iangle} degrees")
+print()
+
+#find angle omega
+omega = ClassicalElements.angleOmega(r, v)
+print(f"omega = {omega} degrees")
+print()
+
+#find angle theta
+theta = ClassicalElements.angleTheta(r, v)
+print(f"theta = {theta} degrees")
+print()
+
+#find angle w
+wangle = ClassicalElements.angleW(r, v)
+print(f"w = {wangle} degrees")
+print()
+
+#coordinate transformations
+
+o = math.radians(omega)
+i = math.radians(iangle)
+w = math.radians(wangle)
+
+rijk = [[4623.458], [5533.656], [4646.733]]
+vijk = [[-6.033], [3.485], [4.480]]
+
+rotationMatrix = [
+    [math.cos(w)*math.cos(o)-math.sin(o)*math.sin(w)*math.cos(i), math.cos(w)*math.sin(o)+math.sin(w)*math.cos(i)*math.cos(o), math.sin(w)*math.sin(i)],
+    [-1*math.sin(w)*math.cos(o)-math.sin(o)*math.cos(w)*math.cos(i), -1*math.sin(w)*math.sin(o)+math.cos(o)*math.cos(w)*math.cos(i), math.cos(w)*math.sin(i)],
+    [math.sin(o)*math.sin(i), -1*math.sin(i)*math.cos(o), math.cos(i)]
+] 
+
+rpqw = calc.matrix_product(rotationMatrix, rijk)
+vpqw = calc.matrix_product(rotationMatrix, vijk)
+
+print(f"r in the perifocal (pqw) frame = {rpqw}")
+print(f"v in the perifocal (pqw) frame = {vpqw}")
